cbc8c28913d8fd00d6492df5a3fb025ccb69c315
John Cokos <highlander.44@gmail.com>
2018-03-23 16:21:24 -0700

    Class 13/14 Scaffold

---
+++ 201/class-13/README.md
@@ -0,0 +1,12 @@
+# Bus Mall Implementation
+
+NOTES:
+
+- Used some sorting (maybe to advanced for a 201?)
+
+
+QUESTIONS:
+- When do we start talking about factoring things out?
+- When do we start introducing conventions like never modifying object properties directly (but using methods instead)?
+- Can we introduce the usage of objects (like I did here) or even sets to track things you've seen?
+-- Or, is it more about having the class learn how to torture arrays so that they have comfort?
---
+++ 201/class-14/js/cart.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var Cart = [];
+
+function loadCart() {
+    Cart = JSON.parse(localStorage.getItem('cart')) || [];
+    console.log(Cart);
+}
+
+// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+function showCart() {
+    
+    // TODO: Find the table
+
+    // TODO: Iterate over the items in the cart
+        // TODO: Create a TR
+        // TODO: Create a TD for the quantity and the item
+        // TODO: Add the TR to the TBODY and both TD's to the TR
+    for( var i in Cart ) { 
+        var tr = document.createElement('tr');
+        var qTD = document.createElement('td');
+            qTD.textContent = Cart[i].quantity;
+        var iTD = document.createElement('td');
+            iTD.textContent = Cart[i].item;
+
+        tableBody.appendChild(tr);
+        tr.appendChild(qTD);
+        tr.appendChild(iTD);
+        console.log("Added Item");
+    }
+}
+
+function removeItemFromCart() {
+    // TODO: When a delete link is clicked, rebuild the Cart array without that item
+    // TODO: Save the cart back to local storage
+    // TODO: Re-draw the cart table
+}
+
+// TODO: Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
+
+
+
+loadCart();
+
+showCart();
