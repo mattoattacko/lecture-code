8cab31b4696994c3ddc2a2db3ded22936c1fa0b1
John Cokos <highlander.44@gmail.com>
2018-09-25 23:37:37 -0700

    test

--- 401-js/refactor/26-react/react-101/src/index.js
+++ 401-js/refactor/26-react/react-101/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './app.js';
+import App from "./app.js";
 
-import createStore from './store/';
+import createStore from "./store/";
 const store = createStore();
 
 class Main extends React.Component {
@@ -20,5 +20,5 @@ class Main extends React.Component {
   }
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<Main />, rootElement);
